b0d43c155ce5f5374123c1806c3152f4576b12d6
Jared Coleman <jaredraycoleman@gmail.com>
2021-04-25 17:44:05 -0700

    adds brute force solver

--- setup.py
+++ setup.py
@@ -4,20 +4,5 @@ setup(
     name='usc.anrg.bnet',
     version='0.0.1',
     license='MIT',
-    url='https://github.com/meejah/python-skeleton',
-
-    install_requires=open('requirements.txt').readlines(),
-    extras_require=dict(
-        dev=open('requirements-dev.txt').readlines()
-    ),
-
-    description='XXX Skeleton python project example.',
-    long_description=open('README.rst', 'r').read(),
-    packages=find_packages(),
-    data_files=[('share/checkout', ['README.rst'])],
-    entry_points=dict(
-        console_scripts=[
-            'bnet=bnet.main:main'
-        ]
-    ),
+    packages=find_packages()
 )
