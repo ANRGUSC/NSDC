1c898f101094c3337fcc8d6df43360465f890dde
Jared Coleman <jaredraycoleman@gmail.com>
2021-08-19 20:53:20 -0700

    Updates optimizer interface and brute force optimizer

--- bnet/optimizers/brute_force.py
+++ bnet/optimizers/brute_force.py
@@ -2,32 +2,38 @@ from bnet.generators.generator import TaskGraphGenerator
 import numpy as np
 from bnet import Network, TaskGraph
 from bnet.schedulers import Scheduler
-from typing import Callable, Generator, Iterable, Tuple
+from typing import Callable, Dict, Generator, Iterable, Tuple, Any
 from .optimizer import Optimizer
 
-from networkx.drawing.nx_agraph import graphviz_layout
-import networkx as nx 
-import matplotlib.pyplot as plt
-
 class BruteForceOptimizer(Optimizer):
-    def __init__(self, samples: int) -> None:
+    def __init__(self, 
+                 networks: Iterable[Network], 
+                 samples: int,
+                 task_graph_generator: TaskGraphGenerator,
+                 cost_func: Callable[..., float],
+                 **metrics: Callable[[Network, TaskGraph], float]) -> None:
         super().__init__()
+        self.networks = networks
         self.samples = samples 
+        self.task_graph_generator = task_graph_generator
+        self.cost_func = cost_func
+        self.metrics = metrics
 
-    def optimize_iter(self, 
-                      networks: Iterable[Network], 
-                      task_graph_generator: TaskGraphGenerator, 
-                      scheduler: Scheduler) -> Generator[Tuple[Network, float, TaskGraph, Network, float], None, None]:
-        min_score, best_network = np.inf, None
-        for network in networks:
-            cum_score = 0
+    def optimize_iter(self) -> Generator[Tuple[Network, float, TaskGraph, Network, float], None, None]:
+        min_cost, best_network = np.inf, None
+        for network in self.networks:
+            cum_cost = 0
             for _ in range(self.samples):
-                task_graph = task_graph_generator.generate()
-                score = scheduler.schedule(task_graph, network)
-
-                cum_score += score
-                yield network, score, task_graph, best_network, min_score
+                task_graph = self.task_graph_generator.generate()
+                metrics = {
+                    metric: func(network, task_graph) 
+                    for metric, func in self.metrics.items()
+                }
+                cost = self.cost_func(**metrics)
+                metrics["cost"] = cost
+                cum_cost += cost
+                yield network, metrics, task_graph, best_network, min_cost
             
-            avg_score = cum_score / self.samples
-            if avg_score < min_score:
-                min_score, best_network = avg_score, network 
+            avg_cost = cum_cost / self.samples
+            if avg_cost < min_cost:
+                min_cost, best_network = avg_cost, network 
--- bnet/optimizers/optimizer.py
+++ bnet/optimizers/optimizer.py
@@ -1,26 +1,31 @@
 from abc import ABC, abstractmethod
-from bnet.generators.generator import TaskGraphGenerator
 from bnet.task_graph import TaskGraph
-from typing import Iterable, Tuple, Generator
+from typing import Any,  Tuple, Generator
 from bnet import Network
-from bnet.schedulers import Scheduler
-
 
 class Optimizer(ABC):
     def __init__(self) -> None:
         super().__init__()
 
     @abstractmethod
-    def optimize_iter(self, 
-                      networks: Iterable[Network], 
-                      task_graph_generator: TaskGraphGenerator, 
-                      scheduler: Scheduler) -> Generator[Tuple[Network, float, TaskGraph, Network, float], None, None]:
+    def optimize_iter(self) -> Generator[Tuple[Network, Any, TaskGraph, Network, Any], None, None]:
+        """Optimize iterator which yields itermediate results 
+    
+        Yields:
+            Tuple: tuple of five elements: (current network being considered, \
+                metric(s) for current and/or current task graph, current task \
+                graph being considered, best network found so far, metric(s) \
+                for best network found so far)
+        """
         pass
 
-    def optimize(self,
-                 networks: Iterable[Network], 
-                 task_graph_generator: TaskGraphGenerator, 
-                 scheduler: Scheduler) -> Tuple[Network, float]:
-        for *_, best_network, best_score in self.optimize_iter(networks, task_graph_generator, scheduler):
+    def optimize(self) -> Tuple[Network, Any]:
+        """Runs optimization and returns best result/metrics
+        
+        Returns:
+            Tuple: tuple of two elements: (best network found so far, \
+                metric(s) for best network found so far)
+        """
+        for *_, best_network, best_score in self.optimize_iter():
             pass 
         return best_network, best_score
