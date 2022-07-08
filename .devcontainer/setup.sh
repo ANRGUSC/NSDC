e513de64dbfdaedcf7b76f616970bd8ca83e646c
Jared Ray Coleman <jaredraycoleman@gmail.com>
2022-07-08 03:01:45 +0000

    Updates setup scripts

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+git submodule update --recursive --remote
+
 pip install --upgrade pip
 pip install -e ./nsdc_framework
+pip install -e ./wfcommons
